5033b92caf5c433cab9048d2be4791d0921d7a78
KawaCoder <67145585+KawaCoder@users.noreply.github.com>
2026-03-29 18:57:19 +0200

    cgt nom db

--- config/env.php
+++ config/env.php
@@ -4,7 +4,7 @@
 
 // -- BASE DE DONNÉES (AlwaysData) --
 define('DB_HOST', 'mysql-back.alwaysdata.net');
-define('DB_NAME', 'back'); // Habituellement sur AlwaysData, le nom de la BDD est le même que le user par défaut
+define('DB_NAME', 'back_db');
 define('DB_USER', 'back');
 define('DB_PASSWORD', '!UBq69!G2rel');
 
